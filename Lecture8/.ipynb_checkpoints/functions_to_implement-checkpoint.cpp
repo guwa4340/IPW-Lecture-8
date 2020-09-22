97e87f2fe1a8be232667e55089960037e3a87eb5
guwa4340 <gwallach98@gmail.com>
2020-09-22 01:30:45 +0000

    Changed for Push

--- Lecture8/.ipynb_checkpoints/functions_to_implement-checkpoint.cpp
+++ Lecture8/.ipynb_checkpoints/functions_to_implement-checkpoint.cpp
@@ -120,12 +120,7 @@ std::vector<int> AddN(std::vector<int> v, int n){
 }
 
 // adds n to each element of the vector
-std::vector<double> AddN(std::vector<double> v, double n){
-    for(int i=0;i<(double)v.size();i++){
-        v[i]+=n;
-    }
-    return v;
-}
+std::vector<double> AddN(std::vector<double> v, double n);
 
 // adds n to each element of the vector
 std::vector<std::string> AddN(std::vector<std::string> v, std::string n);
