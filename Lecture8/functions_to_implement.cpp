fdc04bf9e5775657f3d2d4e038757e7bc4fd9e59
guwa4340 <gwallach98@gmail.com>
2020-09-22 04:56:34 +0000

    Travis Attempt

--- Lecture8/functions_to_implement.cpp
+++ Lecture8/functions_to_implement.cpp
@@ -121,7 +121,7 @@ std::vector<int> AddN(std::vector<int> v, int n){
 
 // adds n to each element of the vector
 std::vector<double> AddN(std::vector<double> v, double n){
-    for (int i=0;i<(double)v.size();i++){
+    for(int i=0;i<(double)v.size();i++){
         v[i]+=n;
     }
     return v;
@@ -140,9 +140,4 @@ std::vector<int> SubtractN(std::vector<int> v, int n){
 }
 
 // subtracts n to each element of the vector
-std::vector<double> SubtractN(std::vector<double> v, double n){
-    for (int i =0;i<(double)v.size();i++){
-        v[i]-=n;
-    }
-    return v;
-}
+std::vector<double> SubtractN(std::vector<double> v, double n);
